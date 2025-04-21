fb2b6b4de908b2740d896fd9979a80de5e82f364
mmucahitOt <m.mucahit0@gmail.com>
2025-04-21 19:41:28 +0300

    unicafe step 4 is implemented

---
+++ part_1/component_state_and_event_handlers/1_9_unicafe/unicafe/src/views/feedBack/components/FeedbackStatistics.jsx
@@ -0,0 +1,47 @@
+const FeedbackStatistics = ({ goodCount, neutralCount, badCount }) => {
+  if (goodCount === 0 && neutralCount === 0 && badCount === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {goodCount}</p>
+      <p>Neutral: {neutralCount}</p>
+      <p>Bad: {badCount}</p>
+      <p>All: {goodCount + neutralCount + badCount}</p>
+      <p>Average: {calculateAverage(goodCount, neutralCount, badCount)}</p>
+      <p>
+        Positive:{" "}
+        {calculatePositivePercentage(goodCount, neutralCount, badCount)}
+        {"%"}
+      </p>
+    </div>
+  );
+};
+
+const calculateAverage = (goodCount, neutralCount, badCount) => {
+  const feedbackCount = goodCount + neutralCount + badCount;
+  if (feedbackCount === 0) {
+    return 0;
+  }
+  const totalCount = goodCount * 1 + neutralCount * 0 + badCount * -1;
+  const average = totalCount / feedbackCount;
+  return average;
+};
+
+const calculatePositivePercentage = (goodCount, neutralCount, badCount) => {
+  const feedbackCount = goodCount + neutralCount + badCount;
+  if (feedbackCount === 0) {
+    return 0;
+  }
+  const positivePercentage = (goodCount / feedbackCount) * 100;
+  return positivePercentage;
+};
+
+export default FeedbackStatistics;
