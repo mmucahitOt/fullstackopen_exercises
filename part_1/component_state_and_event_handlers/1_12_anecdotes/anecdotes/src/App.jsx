abd02592cfa60310444c7397f13b74ff30695a02
mmucahitOt <m.mucahit0@gmail.com>
2025-04-22 08:45:14 +0300

    anecdote step 1 is implemented

---
+++ part_1/component_state_and_event_handlers/1_12_anecdotes/anecdotes/src/App.jsx
@@ -0,0 +1,11 @@
+import Anecdote from "./views/Anecdote";
+
+const App = () => {
+  return (
+    <div>
+      <Anecdote />
+    </div>
+  );
+};
+
+export default App;
---
+++ part_1/component_state_and_event_handlers/1_12_anecdotes/anecdotes/src/components/index.jsx
@@ -0,0 +1,3 @@
+export { default as Button } from "./Button";
+export { default as Text } from "./statics/Text";
+export { default as Table } from "./statics/Table";
