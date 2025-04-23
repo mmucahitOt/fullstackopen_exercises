e1ffb7e29ce1185aabf2e7f3c9fd3d6ee6defc50
mmucahitOt <m.mucahit0@gmail.com>
2025-04-23 19:19:15 +0300

    course information step 8 is implemented

---
+++ part_2/rendering_a_collection_modules/2_3_course_information/course_information/src/App.jsx
@@ -0,0 +1,32 @@
+import Course from "./views/Course";
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        id: 1,
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        id: 2,
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        id: 3,
+        name: "State of a component",
+        exercises: 14,
+      },
+      {
+        id: 4,
+        name: "Redux",
+        exercises: 11,
+      },
+    ],
+  };
+  return <Course course={course} />;
+};
+
+export default App;
---
+++ part_2/rendering_a_collection_modules/2_3_course_information/course_information/src/views/components/Content/Part.jsx
@@ -0,0 +1,11 @@
+const Part = ({ part }) => {
+  return (
+    <>
+      <p key={part.id}>
+        {part.name} {part.exercises}
+      </p>
+    </>
+  );
+};
+
+export default Part;
