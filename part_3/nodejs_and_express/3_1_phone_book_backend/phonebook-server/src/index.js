d005c9b69618cb4fa43ef9640d997a271a44b02e
mmucahitOt <m.mucahit0@gmail.com>
2025-04-30 21:11:45 +0300

    phonebook-backend step 2 is implemented

--- part_3/nodejs_and_express/3_1_phone_book_backend/phonebook-server/src/index.js
+++ part_3/nodejs_and_express/3_1_phone_book_backend/phonebook-server/src/index.js
@@ -10,6 +10,14 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date.toString()}</p>`
+  );
+});
+
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
