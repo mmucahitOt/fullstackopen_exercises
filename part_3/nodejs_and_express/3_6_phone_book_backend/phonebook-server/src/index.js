5b13998b831eff30603e22f5e1a98ec74c66eb80
mmucahitOt <m.mucahit0@gmail.com>
2025-05-01 20:01:09 +0300

    phonebook-backend step 6 is implemented

---
+++ part_3/nodejs_and_express/3_6_phone_book_backend/phonebook-server/src/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const personService = require("./data/person-service");
+const personApiValidator = require("./validators/person-api.validator");
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(personService.getAll());
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+  const persons = personService.getAll();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date.toString()}</p>`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = personService.getById(id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  console.log("id", id);
+  personService.deletePerson(id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  if (!personApiValidator.validateCreatePerson(request, response)) {
+    return;
+  }
+
+  const body = request.body;
+  const person = personService.addPerson(body.name, body.number);
+  response.json(person);
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
